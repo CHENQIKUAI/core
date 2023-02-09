c645f9d5c6afdf2cd780b27ee8ffd2f55e80c02b
cqk <cqk@arcvideo.com>
2023-02-09 14:36:16 +0800

    时间循环

--- code/eventloop.js
+++ code/eventloop.js
@@ -1,14 +1,24 @@
-console.log(1)
+console.log('---1')
 Promise.resolve(1).then(() => {
-  console.log(3)
+  console.log('---3')
+  setTimeout(() => {
+    console.log('+++2')
+  })
   Promise.resolve(1).then(() => {
-    console.log(5)
+    console.log('---5')
+    setTimeout(() => {
+      console.log('+++3')
+    })
   })
 })
 Promise.resolve(1).then(() => {
-  console.log(4)
+  console.log('---4')
   Promise.resolve(1).then(() => {
-    console.log(6)
+    console.log('---6')
   })
 })
-console.log(2)
+console.log('---2')
+
+setTimeout(() => {
+  console.log('+++1')
+})
